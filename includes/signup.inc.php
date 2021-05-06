55ff1494b4d8b1980a99371bec9418b7e815e18b
03sid-sharma <03sid.sharma@gmail.com>
2021-05-06 17:03:51 +0530

    prepared statement insert

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,10 +7,20 @@
     $uid = mysqli_real_escape_string($conn,$_POST['uid']);
     $pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
     
-
     $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
-            VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
-    $result = mysqli_query($conn, $sql);
+            VALUES (?,?,?,?,?);";
+
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql))
+        echo "SQL statement failed";
+    else{
+        mysqli_stmt_bind_param($stmt,"sssss", $first, $last, $email, $uid, $pwd);
+        mysqli_stmt_execute($stmt);
+    }
+
+    // $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
+    //         VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
+    // $result = mysqli_query($conn, $sql);
 
     header("Location: ../index.php?signup=success");
 ?>
