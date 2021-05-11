9c02121e864f1aadab8c7a5f1287d0c8984d2cac
03sid-sharma <03sid.sharma@gmail.com>
2021-05-11 17:03:58 +0530

    GET single category implemented

--- php_rest_myblog/models/Category.php
+++ php_rest_myblog/models/Category.php
@@ -28,4 +28,26 @@
 
             return $stmt;
         }
+
+        // GET single category request
+        public function read_single(){
+            // Create query
+            $query = 'SELECT id, name, created_at FROM ' . $this->table . ' WHERE id = ? LIMIT 0,1';
+
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind ID
+            $stmt->bindParam(1, $this->id);
+
+            // Execute the query
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // Set properties
+            $this->id = $row['id'];
+            $this->name = $row['name'];
+        }
+
     } 
